44da4faf22c4ffc319c236cbcafba46ad6033102
alvin.wang <alvin.wang@binance.com>
2022-05-04 09:30:32 +0200

    fix: commit imagemin

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="flex-c">
       <Header />
+      <img
+        src={new URL('./logo.png', import.meta.env.VITE_IMG_BASE_URL).href}
+      />
+
       <button
         bg="blue-400 hover:blue-800 dark:blue-500 dark:hover:blue-600"
         text="sm white"
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import viteEslint from 'vite-plugin-eslint';
 import viteStylelint from '@amatlash/vite-plugin-stylelint';
 // vite.config.ts
 import svgr from 'vite-plugin-svgr';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // 是否为生产环境，在生产环境一般会注入 NODE_ENV 这个环境变量，见下面的环境变量文件配置
 const isProduction = process.env.NODE_ENV === 'production';
@@ -43,9 +44,34 @@ export default defineConfig({
       // 注意: 对于 emotion，需要单独加上这个配置
       // 通过 `@emotion/react` 包编译 emotion 中的特殊 jsx 语法
       jsxImportSource: '@emotion/react'
+    }),
+    viteImagemin({
+      // 无损压缩配置，无损压缩下图片质量不会变差
+      optipng: {
+        optimizationLevel: 7
+      },
+      // 有损压缩配置，有损压缩下图片质量可能会变差
+      pngquant: {
+        quality: [0.8, 0.9]
+      },
+      // svg 优化
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
     })
   ],
-
+  build: {
+    // 8 KB
+    assetsInlineLimit: 8 * 1024
+  },
   resolve: {
     // 别名配置
     alias: {
